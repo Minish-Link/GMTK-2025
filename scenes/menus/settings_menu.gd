c059fed74c17a0f85d92c6648df7b87394aa4efb
etymonym <144534091+etymonym@users.noreply.github.com>
2025-08-03 09:50:55 -0500

    Setting Impermanence Fix

--- scenes/menus/settings_menu.gd
+++ scenes/menus/settings_menu.gd
@@ -8,22 +8,29 @@ extends Control
 var main_bus_index = AudioServer.get_bus_index("Master")
 var music_bus_index = AudioServer.get_bus_index("Music")
 var sfx_bus_index = AudioServer.get_bus_index("SFX")
+var default_settings: Dictionary = {
+	"main_volume": 0.3,
+	"music_volume": 0.3,
+	"sfx_volume": 0.3,
+	"color_blind": 0
+}
+var settings_data = default_settings
 
-var settings_data = JSON.new()
 var settings_changed: bool = false
 
 func _ready() -> void:
-	var error = settings_data.parse(FileAccess.get_file_as_string("user://settings.json"))
-	if error == OK:
-		settings_data = settings_data.get_data()
-		main_volume.value = settings_data["main_volume"]
-		music_volume.value = settings_data["music_volume"]
-		sfx_volume.value = settings_data["sfx_volume"]
-		
-		color_blind_setting.select(settings_data["color_blind"])
+	if not FileAccess.file_exists("user://settings.json"):
+		var settings_file = FileAccess.open("user://settings.json", FileAccess.WRITE)
+		settings_file.store_string(JSON.stringify(settings_data))
 	else:
-		#settings_data = {}
-		_on_reset_button_pressed()
+		settings_data = JSON.parse_string(FileAccess.get_file_as_string("user://settings.json"))
+	
+	main_volume.value = settings_data["main_volume"]
+	music_volume.value = settings_data["music_volume"]
+	sfx_volume.value = settings_data["sfx_volume"]
+		
+	color_blind_setting.select(settings_data["color_blind"])
+	
 	
 func _input(_ev):
 	if Input.is_key_pressed(KEY_ESCAPE):
@@ -66,13 +73,12 @@ func _on_sfx_slider_value_changed(value: float) -> void:
 
 
 func _on_reset_button_pressed() -> void:
-	settings_data["main_volume"] = 0.3
-	settings_data["music_volume"] = 0.3
-	settings_data["sfx_volume"] = 0.3
-	settings_data["color_blind"] = 0
-	main_volume.value = settings_data["main_volume"]
-	music_volume.value = settings_data["music_volume"]
-	sfx_volume.value = settings_data["sfx_volume"]
-	color_blind_setting.select(settings_data["color_blind"])
-	settings_changed = true
+	if settings_data != default_settings:
+		settings_changed = true
+		settings_data = default_settings
+		main_volume.value = settings_data["main_volume"]
+		music_volume.value = settings_data["music_volume"]
+		sfx_volume.value = settings_data["sfx_volume"]
+		color_blind_setting.select(settings_data["color_blind"])
+
 	%ClickAudio.play()
